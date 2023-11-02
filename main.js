8a75856b1b15fcf152717af21802c5dbd4f303c0
Emanuel Ybalo <brianybalo70@gmail.com>
2023-11-02 17:05:06 -0300

    actualizacion de colores y implementacion de js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+// // modal
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+});
+
+// Boton Oscuro / Claro
+
+const boton = document.getElementById('boton-oc');
+
+boton.addEventListener('change', () => {
+  const body = document.body;
+
+  if (body.style.backgroundColor === 'rgb(1, 22, 30)' || body.style.backgroundColor === '#e8e8e8') {
+    console.log('Cambiar a oscuro');
+    body.style.backgroundColor = '#e8e8e8';
+  } else {
+    console.log('Cambiar a claro');
+    body.style.backgroundColor = '#01161e';
+  }
+});
 
