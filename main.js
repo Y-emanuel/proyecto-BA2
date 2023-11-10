9156fa64f97432a5e0c4b7627bd2e2eeb6b3acb9
Emanuel Ybalo <brianybalo70@gmail.com>
2023-11-10 16:46:19 -0300

    inplementacion de boton whatsapp

--- main.js
+++ main.js
@@ -1,24 +1,59 @@
-// // modal
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
+// Función para mostrar la alerta
+function mostrarAlerta() {
+  let alerta = document.querySelector(".alerta-overlay");
+  alerta.style.display = "flex";
+  setTimeout(() => {
+    alerta.style.opacity = 1;
+  }, 10);
+}
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-});
+// Función para cerrar la alerta
+function cerrarAlerta() {
+  let alerta = document.querySelector(".alerta-overlay");
+  alerta.style.opacity = 0;
+  setTimeout(() => {
+    alerta.style.display = "none";
+  }, 500);
+}
+document.getElementById("hot").addEventListener("click", mostrarAlerta);
+document.getElementById("dep").addEventListener("click", mostrarAlerta);
+document.getElementById("alq").addEventListener("click", mostrarAlerta);
+document.getElementById("act").addEventListener("click", mostrarAlerta);
+document.getElementById("cerrarAlerta").addEventListener("click", cerrarAlerta);
+document.getElementById("adquirir").addEventListener("click", cerrarAlerta);
 
 // Boton Oscuro / Claro
 
-const boton = document.getElementById('boton-oc');
+let boton = document.getElementById("boton-oc");
 
-boton.addEventListener('click', () => {
+boton.addEventListener("click", () => {
   const body = document.body;
 
-  if (body.style.backgroundColor === 'rgb(1, 22, 30)' || body.style.backgroundColor === '#e8e8e8') {
-    console.log('Cambiar a oscuro');
-    body.style.backgroundColor = '#e8e8e8';
+  if (
+    body.style.backgroundColor === "rgb(1, 22, 30)" ||
+    body.style.backgroundColor === "#e8e8e8"
+  ) {
+    console.log("Cambiar a oscuro");
+    body.style.backgroundColor = "#e8e8e8";
   } else {
-    console.log('Cambiar a claro');
-    body.style.backgroundColor = '#01161e';
+    console.log("Cambiar a claro");
+    body.style.backgroundColor = "#01161e";
   }
 });
 
+const abrirWhatsapp = () => {
+  let cuadro = document.getElementById("cuadro");
+  let triangulo = document.getElementById("triangulo-w");
+
+  if (cuadro.style.opacity == 0 && triangulo.style.opacity == 0) {
+    cuadro.style.opacity = 1;
+    triangulo.style.opacity = 1;
+  } else {
+    cuadro.style.opacity = 0;
+    triangulo.style.opacity = 0;
+  }
+};
+
+document
+  .getElementById("click-boton-w")
+  .addEventListener("click", abrirWhatsapp);
